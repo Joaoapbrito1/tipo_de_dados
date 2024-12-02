b7bb2ffb2cf9afeb7589f7edb264ca2654d45967
Joaoapbrito1 <joao.brito@zup.com.br>
2024-12-02 16:12:35 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 10; // Tipo Inteiro (Tipo primitivo)
+        double numeroDecimal = 10.00; // Numero decimal (Tipo primitivo)
+        long numeroLongo = 12345678912l; // Numero logo (Tipo primitivo)
+        float numeroFlutuante = 12.123456f; // Numero flutuante (Tipo primitivo)
+
+        boolean boleano = true; // Boleano (Tipo primitivo) - true ou false
+        char caracter = 'M'; // Caracteres individuais (Tipo primitivo)
+
+        String frase = "Catalisa e muito legal"; // Cadeia de caracteres (Tipo de referencia)
+
+        int numeroConvertido = (int)11.00; // Conversão casting
+
+    }
+}
